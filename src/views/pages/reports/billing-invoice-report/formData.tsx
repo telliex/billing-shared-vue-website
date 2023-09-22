f5b031e95d95c052bffa976ba3c6002310195e3d
eva.yang <eva.yang@ecloudvalley.com>
2023-09-22 06:57:39 +0000

    RSI-807  MGT會計報表搬移至MARS作業

---
+++ src/views/pages/reports/billing-invoice-report/formData.tsx
@@ -0,0 +1,39 @@
+import { FormSchema } from '/@/components/Form/index';
+import { useI18n } from '/@/hooks/web/useI18n';
+const { t } = useI18n();
+
+export function getFormSchema(): FormSchema[] {
+  return [
+    {
+      field: 'ReportType',
+      component: 'Select',
+      label: `${t('report.dopCost.searchAreaReportTypeLavel')}:`,
+      show: false,
+      componentProps: {
+        options: [
+          {
+            label: 'Invoice Report',
+            value: 'billing_invoice_report_hk',
+            key: 'billing_invoice_report_hk',
+          },
+        ],
+      },
+      colProps: {
+        span: 6,
+      },
+    },
+    {
+      field: 'YearMonth',
+      component: 'DatePicker',
+      label: `${t('report.dopCost.searchAreaYYYYMMLavel')}:`,
+      labelWidth: 100,
+      required: true,
+      componentProps: {
+        picker: 'month',
+      },
+      colProps: {
+        span: 6,
+      },
+    },
+  ];
+}
---
+++ src/views/pages/reports/sap-cno-report/formData.tsx
@@ -0,0 +1,39 @@
+import { FormSchema } from '/@/components/Form/index';
+import { useI18n } from '/@/hooks/web/useI18n';
+const { t } = useI18n();
+
+export function getFormSchema(): FormSchema[] {
+  return [
+    {
+      field: 'ReportType',
+      component: 'Select',
+      label: `${t('report.dopCost.searchAreaReportTypeLavel')}:`,
+      show: false,
+      componentProps: {
+        options: [
+          {
+            label: 'SAP_CNO對照表',
+            value: 'sap_cno_contrast_table',
+            key: 'sap_cno_contrast_table',
+          },
+        ],
+      },
+      colProps: {
+        span: 6,
+      },
+    },
+    {
+      field: 'YearMonth',
+      component: 'DatePicker',
+      label: `${t('report.dopCost.searchAreaYYYYMMLavel')}:`,
+      labelWidth: 100,
+      required: true,
+      componentProps: {
+        picker: 'month',
+      },
+      colProps: {
+        span: 6,
+      },
+    },
+  ];
+}
